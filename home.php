d10950365b67e3bc75e92e8da39ec465862a3be9
Ritajamaitra <75163189+Ritajamaitra@users.noreply.github.com>
2023-06-10 09:53:35 +0530

    home page

---
+++ home.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+<style>
+</style>
+</head>
+<body>
+<div class="header">
+<?php
+$active="home";
+include('head.php'); ?>
+
+</div>
+<?php include'ticker.php'; ?>
+ <body style="background-color:rgb(210,180,140);">
+<div id="page-container" style="margin-top:50px; position: relative;min-height: 84vh;   ">
+    <div class="container">
+    <div id="content-wrap"style="padding-bottom:75px;">
+  <!--<div id="demo" class="carousel slide" data-ride="carousel">-->
+
+    <!-- Indicators 
+    <ul class="carousel-indicators">
+      <li data-target="#demo" data-slide-to="0" class="active"></li>
+      <li data-target="#demo" data-slide-to="1"></li>
+    </ul>
+
+   
+
+    </div>-->
+
+    <!-- Left and right controls 
+    <a class="carousel-control-prev" href="#demo" data-slide="prev">
+      <span class="carousel-control-prev-icon"></span>
+    </a>
+    <a class="carousel-control-next" href="#demo" data-slide="next">
+      <span class="carousel-control-next-icon"></span>
+    </a>
+  </div>-->
+<br>
+        <h1 style="text-align:center;font-size:45px;">Help LOINC Standardize the Names of Indian Lab Tests </h1>
+<br>
+        
+                        
+                </div>
+            </div>
+            
+
+        <h2></h2>
+                
+        
+
+  <div class="footer">
+<?php
+$active="home";
+include('footer.php'); ?>
+
+</div>
+</body>
+
+</html>
