8cf0d1d6e8f31c842bad2d246ab5a28948c66f12
Ritajamaitra <75163189+Ritajamaitra@users.noreply.github.com>
2023-08-23 21:19:56 +0530

    Update list.php

--- list.php
+++ list.php
@@ -1,38 +1,92 @@
 <html>
-<head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <meta name="description" content="">
-  <meta name="author" content="">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 
-</head>
+<head>
+
+
 <style>
 
-#sidebar{position:relative;margin-top:-20px}
-#content{position:relative;margin-left:210px}
-@media screen and (max-width: 600px) {
-  #content {
-    position:relative;margin-left:auto;margin-right:auto;
+.header {
+  overflow:hidden;
+  background-color: #333;
+  top: 0;
+  width:100%;
+  padding: 10px 5px;
+  color:#FF0404  ;
+}
+
+/* Style the header links */
+.header a {
+  float: left;
+  color: white;
+  text-align: center;
+  padding: 12px;
+  text-decoration: none;
+  font-size: 18px;
+  line-height: 25px;
+  border-radius: 4px;
+  font-weight:bold;
+}
+
+/* Style the logo link (notice that we set the same value of line-height and font-size to prevent the header to increase when the font gets bigger */
+.header a.logo {
+  font-size: 25px;
+  font-weight: bold;
+  color:#FF0404  ;
+}
+
+/* Change the background color on mouse-over */
+.header a:hover {
+  background-color: #ddd;
+  color: black;
+}
+
+.center {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    height: 150px;
+  }
+
+  .custom-button {
+    background-color: #f44336;
+    color: white;
+    padding: 20px;
+    border: none;
+    border-radius: 4px;
+    cursor: pointer;
+    font-size: 24px;
+  }
+
+
+/* Float the link section to the right */
+.header-right {
+  float: right;
+}
+
+/* Add media queries for responsiveness - when the screen is 500px wide or less, stack the links on top of each other */
+@media screen and (max-width: 500px) {
+  .header a {
+    float: none;
+    display: block;
+    text-align: left;
+  }
+  .header-right {
+    float: none;
   }
 }
-  #he{
-      font-size: 14px;
-      font-weight: 600;
-      text-transform: uppercase;
-      padding: 3px 7px;
-      color: #fff;
-      text-decoration: none;
-      border-radius: 3px;
-      align:center
-  }
+/* Style the active/current link*/
+a.act{
+background: linear-gradient(to right, #fd746c 0%, #ff9068 100%);
+color: white;
+border-radius:30px;
+}
+a.logo2{
+  background-color: #333;
+}
 </style>
+</head>
 <body>
-
-
 <?php 
 $active ='list';
 include('head.php');
@@ -44,54 +98,26 @@ include('head.php');
       <div class="row">
         <div class="col-md-12 lg-12 sm-12">
 
-          <h1 class="page-title">Indian Medical Laboratory Tests</h1>
+          <h1 class="page-title">Indian Medical Laboratory Tests </h1>
 
         </div>
 
       </div>
-      <hr>
-      <?php
-        include 'conn.php';
-
-        $limit = 500;
-        if(isset($_GET['page'])){
-          $page = $_GET['page'];
-        }else{
-          $page = 1;
-        }
-        $offset = ($page - 1) * $limit;
-        $count=$offset+1;
-        $sql= "select * from indiantest LIMIT {$offset},{$limit}";
-        $result=mysqli_query($conn,$sql);
-        if(mysqli_num_rows($result)>0)   {
-       ?>
-          <h3 class="table-title">Table 1: List of diagnostic tests at village level </h3>
-
-       <div class="table-responsive">
-      <table class="table table-bordered" style="text-align:center">
-          <thead style="text-align:center">
-          <th style="text-align:center">S.no</th>
-          <th style="text-align:center">Test Category</th>
-          <th style="text-align:center">Diagnostic Test</th>
-          <th style="text-align:center">Specimen Type</th>
-          <th style="text-align:center">Equipment used for test</th>
-         
-          </thead>
-          <tbody>
-            <?php
-            while($row = mysqli_fetch_assoc($result)) { ?>
-          <tr>
-                  <td><?php echo $count++; ?></td>
-                  <td><?php echo $row['Test_category']; ?></td>
-                  <td><?php echo $row['Diagnostic_Test']; ?></td>
-                  <td><?php echo $row['Specimen_Type']; ?></td>
-                  <td><?php echo $row['Equipments_used']; ?></td>
-                   
-                </td>
-              </tr>
-            <?php } ?>
-          </tbody>
-      </table>
-      
-    </div>
-    <?php } ?>
+     
+      <div class="center">
+  <a href="list1.php">
+    <button class="custom-button">Indian Medical Laboratory Tests at Villages<br>and Subcentres</button>
+  </a>
+</div>
+<div class="center">
+  <a href="list2.php">
+    <button class="custom-button">Indian Medical Laboratory Tests at <br> Primary and Community Health Centres</button>
+  </a>
+</div>
+<div class="center">
+  <a href="list3.php">
+    <button class="custom-button">Indian Medical Laboratory Tests at <br> Sub-District and District Health Centres</button>
+  </a>
+</div>
+</body>
+</html>
